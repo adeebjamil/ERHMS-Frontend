91543f5d38ad7592912b0426466bdf8b87aa1eb9
Your Name <youremail@example.com>
2025-04-17 14:53:39 +0530

    up-3

---
+++ dist/assets/constants-C-b4r1xk.js
@@ -0,0 +1,1 @@
+const o="/img1.jpg",s={name:"Lovosis technonogy private limited",shortName:"Lovosis",logo:"/company-logo.png"};export{s as C,o as P};
---
+++ dist/assets/helpers-CaOlnEck.js
@@ -0,0 +1,1 @@
+const t=r=>r?r.startsWith("http")?r:`${"https://ehrms-backend.onrender.com/api".replace("/api","")}${r}`:null;export{t as g};
